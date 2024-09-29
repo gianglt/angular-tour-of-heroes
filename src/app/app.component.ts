f4b7170602e4631bf214cceb2f0abdee07c74072
Giang Lam <gianglt@gmail.com>
2024-09-29 17:27:03 +0700

    AppConfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MessagesComponent } from './messages/messages.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {provideHttpClient} from "@angular/common/http";
 import { routes } from './app.routes';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom([
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
+    ])
+  ]
 };
